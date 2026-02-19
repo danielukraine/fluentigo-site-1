f5b20d5b94cdd51ed3fe1278888e6f1058e666e4
Daniil Kononenko <102914915+danielukraine@users.noreply.github.com>
2026-02-19 23:14:49 +0100

    Remove BrowserRouter and simplify routing

--- TribnikovSite/src/App.tsx
+++ TribnikovSite/src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { BookingDialogProvider } from "@/components/booking/BookingDialogProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -15,13 +15,10 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BookingDialogProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </BookingDialogProvider>
     </TooltipProvider>
   </QueryClientProvider>
